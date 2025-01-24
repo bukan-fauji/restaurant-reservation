10d4d488cbf066737a0d79a3ec8f1bad3e5b9d43
bukan-fauji <fauji@students.amikom.ac.id>
2025-01-24 14:54:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'restaurant_db'
+    'host': os.getenv('DB_HOST', 'mysql://root:noeiMupVeGBGiMXowKzPfQMElrrvTDWK@monorail.proxy.rlwy.net:28653/railway'),
+    'user': os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASSWORD', 'noeiMupVeGBGiMXowKzPfQMElrrvTDWK'),
+    'database': os.getenv('DB_NAME', 'railway')
 }
