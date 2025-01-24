5d6f026ea22c6bf646b0b22e8f210ef54760dea7
bukan-fauji <fauji@students.amikom.ac.id>
2025-01-24 15:46:52 +0700

    Update db.py

--- models/db.py
+++ models/db.py
@@ -2,4 +2,15 @@ import mysql.connector
 from config import DB_CONFIG
 
 def connect_db():
-    return mysql.connector.connect(**DB_CONFIG)
+    try:
+        conn = mysql.connector.connect(
+            host=DB_CONFIG['host'],
+            user=DB_CONFIG['user'],
+            password=DB_CONFIG['password'],
+            database=DB_CONFIG['database'],
+            port=DB_CONFIG['port']
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
